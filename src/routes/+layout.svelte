8a712f1cf7280d3a1438473f7b1d00fb08b62019
Matthew E <122356019+MatthewEvan@users.noreply.github.com>
2023-05-23 08:41:41 +0000

    Adding aliases and configuring components

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<svelte:head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;800&display=swap" rel="stylesheet">
+    <title>Formula 1 Introduction</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+</svelte:head>
+
+<slot/>
+
+<style>
+    :global(*, *::before, *::after) {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-size: 14px;
+        font-family: "Inter", sans-serif;
+    }
+    :global(html, body) {
+        width: 100%;
+        height: 100%;
+    }
+    :global(a) {
+        text-decoration: none;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+    import Navbar from "$components/Navbar.svelte"
+</script>
+
+<h1>Welcome to SvelteKait</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Navbar/>
