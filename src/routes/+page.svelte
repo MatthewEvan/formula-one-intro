3090a1c568c761323bf7aeaadfdf15bd39fd5163
Matthew E <122356019+MatthewEvan@users.noreply.github.com>
2023-05-28 06:37:36 +0000

    Testing Svelte-awesome icons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,11 @@
+<script lang="ts">
+    import Icon from "svelte-awesome";
+    import bars from "svelte-awesome/icons/bars";
+    import close from 'svelte-awesome/icons/close';
+
+</script>
+
 <h1>Welcome to SvelteKait</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Icon data={close}/>
 
