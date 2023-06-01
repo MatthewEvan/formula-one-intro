1baeba2d64e72f4908c04f9c31e5487e436249bc
Matthew E <122356019+MatthewEvan@users.noreply.github.com>
2023-06-01 02:56:30 +0000

    Project Structuring of Drivers Page

--- src/routes/drivers/+page.svelte
+++ src/routes/drivers/+page.svelte
@@ -1,1 +1,22 @@
-<p style="color: white">Welcome to the Drivers Section</p>
+<script>
+
+</script>
+
+<div class="main-page">
+    <section class="header">
+        
+    </section>
+    <section class="drivers-grid">
+
+    </section>
+</div>
+
+<style>
+    .main-page {
+        display: flex;
+        align-items: center;
+        flex-direction: column;
+        gap: 4rem;
+        margin: 1rem 4rem 4rem;
+    }
+</style>
