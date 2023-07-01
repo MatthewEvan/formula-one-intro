fb14461400b12659de9600812a443f1821480aa6
Matthew E <122356019+MatthewEvan@users.noreply.github.com>
2023-07-01 08:30:26 +0000

    Redesigning How To Works Header & Overview

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import MercedesW14 from '$assets/main-page/Mercedes-W14.png';
 	import RedBullRB19 from '$assets/main-page/RedBull-RB19.png';
+	import GridLinesDeco from '$assets/main-page/Gridlines Deco.png';
 
 	import Icon from 'svelte-awesome';
 	import angleDown from 'svelte-awesome/icons/angleDown';
@@ -129,124 +130,35 @@
 		<div class="racing-stripes-deco"></div>
 	</section>
 	<section id="howitworks">
-		<div class="how-intro">
-			<div class="how-intro-desc">
-				<h3>How It Works</h3>
-				<p>
-					A regular Formula One Race Weekend usually consists of 3 sessions that's divided into 3
-					days of the weekend, which are Fridays, Saturdays, and Sundays with different excitement
-					and races altogether
-				</p>
-				<ul class="days-of-races">
-					<li>Race Weekend Activities</li>
-					<li><span>1</span>Fridays :<span>FP1 & FP2</span></li>
-					<li><span>2</span>Saturdays:<span>FP3 & Qualifying</span></li>
-					<li><span>3</span>Fridays :<span>Main Race Day</span></li>
-				</ul>
-			</div>
-			<img
-				src="https://cdn.crash.net/styles/article/s3/pa/3243856.0064.jpg?itok=3MaaXpcU"
-				alt="Racing Scenes"
-			/>
-		</div>
-		<div class="practice-quali">
-			<div class="free-practice fp-quali">
-				<div class="main-title">
+		<h3>How It Works</h3>
+		<div class="event-overview">
+			<div class="event-tiles">
+				<div class="event-header">
 					<h4>Free Practice</h4>
-					<p>/friː ˈpræktɪs/</p>
+					<img src={GridLinesDeco} alt="Gridlines Decoration">
+					<p>Every team gets a chance to test their cars on the specific circuit to adjust its settings according to the weather condition there</p>
 				</div>
-				<p>
-					Free Practice are sessions that are used by teams to freely test their car settings and
-					capabilities around the track, to adjust the car to its best before the main race and
-					qualifying starts which will benefit the teams
-				</p>
-				<div class="fp-sessions">
-					<ul>
-						<li><span>1</span>Free Practice 1</li>
-						<li><span>2</span>Free Practice 2</li>
-						<li><span>3</span>Free Practice 3</li>
-					</ul>
-					<img
-						src="https://www.formula1.com/content/dam/fom-website/manual/Misc/2020/2020races/Austria/F1AUT20jm_101.jpg.transform/6col/image.jpg"
-						alt="Free Practice Session"
-					/>
-				</div>
+				<img src="https://d3cm515ijfiu6w.cloudfront.net/wp-content/uploads/2022/04/04101702/lewis-hamilton-leaves-garage-planetf1.jpg" alt="Free Practice Session of Mercedes AMG F1">
 			</div>
-			<div class="qualifying fp-quali">
-				<div class="main-title">
+			<div class="event-tiles">
+				<div class="event-header">
 					<h4>Qualifying</h4>
-					<p>/ˈkwɒlɪfaɪɪŋ/</p>
+					<img src={GridLinesDeco} alt="Gridlines Decoration">
+					<p>The event where every drivers attempted to score the fastest lap time with to determine their starting position the next day</p>
 				</div>
-				<p>
-					Qualifying is a session that is really important because this determines the position of
-					each driver's car in the race before from the first until the twentieth position.
-					Qualifying is also divided into three sessions
-				</p>
-				<div class="quali-sessions">
-					<ul>
-						<li><span>1</span>P20-P16 position determined</li>
-						<li><span>2</span>P15-P11 position determined</li>
-						<li><span>3</span>P10-P1 position determined</li>
-					</ul>
-					<img
-						src="https://img.sportstars.id//2021/07/WK781L/master_7nzC7bz437_1701.JPG"
-						alt="Qualifying Session"
-					/>
+				<img src="https://d3cm515ijfiu6w.cloudfront.net/wp-content/uploads/2023/03/09115251/red-bull-max-verstappen-2022-emilia-romagna-grand-prix-imola-planet-f1.jpg" alt="F1 Qualifying Session">
+			</div>
+			<div class="event-tiles">
+				<div class="event-header">
+					<h4>Grand Prix</h4>
+					<img src={GridLinesDeco} alt="Gridlines Decoration">
+					<p>The main event everyone’s waiting for, where every drivers battle and overtake others to be the first and win the race</p>
 				</div>
+				<img src="https://media.formula1.com/image/upload/f_auto/q_auto/v1682327140/interlagos-start-2022.png.transform/9col/image.png" alt="F1 Main Race - Grand Prix">
 			</div>
 		</div>
-		<div class="main-race">
-			<div class="layout-race">
-				<div class="main-race-desc">
-					<div class="main-title">
-						<h4>Main Race</h4>
-						<p>/meɪn reɪs/</p>
-					</div>
-					<p>
-						The day all Formula One fans are waiting for, which is the race day where all cars race
-						for almost 1+ hours of 50+ laps around a circuit to get the <b>First Place</b>
-					</p>
-					<div class="buttons-race">
-						<a href="/circuits" title="Go to Circuits Information">
-							Circuits
-							<Icon data={externalLink} />
-						</a>
-						<a href="/drivers" title="Go to Drivers Information">
-							Drivers
-							<Icon data={externalLink} />
-						</a>
-					</div>
-				</div>
-				<div class="points-system">
-					<h5>Points System</h5>
-					<div class="points-list">
-						<ul>
-							<li><span>#01</span>25 points</li>
-							<li><span>#02</span>18 points</li>
-							<li><span>#03</span>15 points</li>
-							<li><span>#04</span>12 points</li>
-							<li><span>#05</span>10 points</li>
-						</ul>
-						<ul>
-							<li><span>#06</span>8 points</li>
-							<li><span>#07</span>6 points</li>
-							<li><span>#08</span>4 points</li>
-							<li><span>#09</span>2 points</li>
-							<li><span>#10</span>1 points</li>
-						</ul>
-					</div>
-				</div>
-			</div>
-			<div class="img-race">
-				<img
-					src="https://media.formula1.com/image/upload/content/dam/fom-website/sutton/2022/Bahrain/Sunday/1386703905.jpg"
-					alt="F1 Race 1"
-				/>
-				<img
-					src="https://media.formula1.com/image/upload/f_auto/q_auto/v1681909842/calendar.jpg.transform/9col/image.jpg"
-					alt="F1 Race 2"
-				/>
-			</div>
+		<div class="event-description">
+			anjay
 		</div>
 	</section>
 </div>
@@ -563,280 +475,71 @@
 	#howitworks {
 		display: flex;
 		flex-direction: column;
-		align-items: start;
-		gap: 4rem;
+		align-items: center;
+		gap: 2rem;
 		margin-top: 4rem;
 	}
-
-	#howitworks .how-intro {
-		display: flex;
-		flex-direction: row;
-		align-items: start;
-		gap: 4rem;
-	}
-	#howitworks .how-intro img {
-		flex: 1;
-		width: 30vw;
-		height: auto;
-		border-radius: 0.5rem;
-	}
-	#howitworks .how-intro .how-intro-desc {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		flex: 1;
-		font-weight: 500;
-		gap: 1rem;
-	}
-	#howitworks .how-intro .how-intro-desc h3 {
-		font-size: 4rem;
-		text-align: center;
-		font-weight: 700;
-		font-family: 'Space Grotesk', monospace;
-		line-height: 1;
-		margin-bottom: 1rem;
-		color: hsl(80, 100%, 59%);
-	}
-	.how-intro-desc p {
-		font-size: 1.0625rem;
-		margin-bottom: 0.5rem;
-	}
-	.days-of-races {
-		display: flex;
-		flex-direction: column;
-		align-items: start;
-		gap: 0.75rem;
-	}
-	.days-of-races li {
-		list-style-type: none;
-		font-size: 1.2rem;
-		display: flex;
-		align-items: center;
-	}
-
-	.days-of-races li:nth-child(1) {
-		margin-bottom: 0.5rem;
-		font-size: 1.3rem;
-		font-weight: 700;
-	}
-	.days-of-races li span:nth-child(1) {
-		width: 2.5rem;
-		aspect-ratio: 1/1;
-		display: grid;
-		place-items: center;
-		border-radius: 100%;
-		background-color: #ff1801;
-		font-weight: 900;
-		font-size: 1.5rem;
-		margin-right: 1rem;
-		border: 2px solid hsl(0, 0%, 95%);
-	}
-
-	.days-of-races li span:nth-child(2) {
-		margin-left: 1rem;
-		font-size: 1.5rem;
+	#howitworks > h3 {
+		font-size: 3.5rem;
 		font-weight: 800;
-		color: #ff716f;
+		text-align: center;
 	}
 
-	/* How it works Free Practice & Qualifying Sections */
-	.practice-quali {
+	.event-overview {
 		display: flex;
-		align-items: start;
-		gap: 4rem;
+		align-items: stretch;
+		gap: 1.5rem;
+		padding-bottom: 3rem;
+		border-bottom: 0.25rem solid hsl(0, 0%, 95%);
 	}
-	.free-practice,
-	.qualifying {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		align-items: start;
-		gap: 1rem;
-	}
-
-	.free-practice .fp-sessions img,
-	.qualifying .quali-sessions img {
-		aspect-ratio: 4/3;
-		width: 20vw;
-		border-radius: 1rem;
-		transition: all 350ms ease-in-out;
-	}
-	.free-practice .fp-sessions img:hover,
-	.qualifying .quali-sessions img:hover {
-		scale: 1.05;
-	}
-
-	.main-title {
-		display: flex;
-		flex-direction: column;
-		gap: 0.5rem;
-	}
-	.main-title h4 {
-		font-size: 4rem;
-		line-height: 1;
-		font-family: 'Space Grotesk', monospace;
-		color: hsl(80, 100%, 59%);
-	}
-	.main-title p {
-		font-size: 1.5rem;
-	}
-
-	.fp-quali p {
-		font-size: 1.05rem;
-		font-weight: 600;
-	}
-
-	.fp-sessions,
-	.quali-sessions {
-		display: flex;
-		align-items: start;
-		justify-content: space-between;
-		gap: 2rem;
-	}
-	.fp-sessions ul,
-	.quali-sessions ul {
-		display: flex;
-		flex-direction: column;
-		gap: 0.5rem;
-		list-style-type: none;
-	}
-	.fp-sessions ul li,
-	.quali-sessions ul li {
-		display: flex;
-		align-items: center;
-		gap: 2rem;
-	}
-	.fp-sessions ul li span,
-	.quali-sessions ul li span {
-		width: 2rem;
-		aspect-ratio: 1/1;
-		display: grid;
-		place-items: center;
-		background-color: hsl(80, 100%, 34%);
-		border: 2px solid hsl(0, 0%, 95%);
-		font-weight: 900;
-		font-size: 1.25rem;
-		border-radius: 100%;
-	}
-
-	/* MAIN RACE SECTION  */
-	.main-race {
-		display: flex;
-		flex-direction: row;
-		align-items: start;
-		gap: 2rem;
-		margin-bottom: 4rem;
-	}
-	.layout-race {
-		display: flex;
-		flex: 2;
-		align-items: start;
-		flex-direction: row;
-	}
-
-	.main-race-desc {
-		display: flex;
-		flex-direction: column;
-		gap: 1rem;
-		flex: 1;
-	}
-	.main-race-desc p {
-		font-weight: 500;
-	}
-	.main-race-desc .buttons-race {
-		display: flex;
-		align-items: center;
-		gap: 2rem;
-		margin-top: 1rem;
-	}
-	.buttons-race a {
-		display: flex;
-		align-items: center;
-		gap: 1rem;
-		font-size: 1.2rem;
-		padding: 0.75rem 1.5rem;
-		border-radius: 0.5rem;
-		box-shadow: 1px 1px 8px hsla(0, 0%, 6%, 0.75);
-		transition: all 350ms ease-in-out;
-	}
-	.buttons-race a:nth-child(1) {
-		background-color: #d90b0b;
-	}
-	.buttons-race a:nth-child(2) {
-		background-color: #bf9663;
-	}
-	.buttons-race a:nth-child(1):hover {
-		scale: 1.025;
-		background-color: #e92020;
-	}
-	.buttons-race a:nth-child(2):hover {
-		scale: 1.025;
-		background-color: #e8b980;
-	}
-
-	.points-system {
+	.event-tiles {
 		flex: 1;
 		display: flex;
 		flex-direction: column;
 		align-items: center;
-		gap: 1rem;
-	}
-	.points-system h5 {
-		font-size: 2rem;
-		font-family: 'Space Grotesk', monospace;
-		color: hsl(80, 100%, 59%);
-		font-weight: 400;
-	}
-	.points-list {
-		display: flex;
 		gap: 2rem;
+		padding: 2rem 1.5rem;
+		background-color: hsl(0, 100%, 30%);
+		border-radius: 0.25rem;
 	}
-	.points-list ul {
-		list-style-type: none;
-		display: flex;
-		flex-direction: column;
-		gap: 1rem;
+	.event-tiles:nth-child(2) {
+		background-color: hsl(0, 100%, 40%);
 	}
-	.points-list ul li {
-		display: flex;
-		flex-direction: row;
-		align-items: center;
-		font-size: 1.25rem;
-	}
-	.points-list ul li span {
-		width: 4rem;
-		height: 2.5rem;
-		background-color: hsl(80, 100%, 34%);
-		font-size: 1.25rem;
-		font-weight: 700;
-		display: grid;
-		place-items: center;
-		border-radius: 0.5rem;
-		border: 2px solid hsl(0, 0%, 95%);
-		margin-right: 0.5rem;
-	}
-
-	.img-race {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		gap: 1rem;
-	}
-	.img-race img {
-		width: 30vw;
-		aspect-ratio: 4/1.5;
-		object-fit: cover;
-		object-position: bottom;
+	.event-tiles > img {
+		width: 80%;
+		height: 2/1; /* sementara */
 		border-radius: 0.5rem;
 		opacity: 0.9;
 		transition: all 350ms ease-in-out;
+		object-fit: cover;
 	}
-	.img-race img:hover {
+	.event-tiles > img:hover {
+		scale: 1.025;
 		opacity: 1;
-		scale: 1.05;
 	}
 
+	.event-header {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		gap: 0.5rem;
+	}
+	.event-header h4 {
+		font-size: 2.5rem;
+		font-family: "Space Grotesk", monospace;
+	}
+	.event-header p {
+		text-align: center;
+		font-family: "Space Grotesk", monospace;
+	}
+	.event-header img {
+		width: 85%;
+		height: 3rem;
+		object-fit: cover;
+		margin-bottom: 1rem
+	}
+	
+	
 	@media screen and (max-width: 1024px) {
 		.main-page {
 			margin: 2rem;
@@ -869,36 +572,23 @@
 		#howitworks {
 			margin-inline: 3rem;
 		}
-		#howitworks .how-intro {
+		.event-overview {
 			flex-direction: column;
-			align-items: center;
-			gap: 2rem;
 		}
-		#howitworks .how-intro img {
-			width: 80vw;
-		}
-		.practice-quali {
-			flex-direction: column;
-			align-items: center;
-		}
-		.free-practice .fp-sessions img,
-		.qualifying .quali-sessions img {
-			aspect-ratio: 21/9;
-			width: 60vw;
-			object-fit: cover;
-			object-position: bottom;
-		}
-
-		.main-race {
-			flex-direction: column;
-			align-items: center;
-		}
-		.img-race {
-			margin-top: 2rem;
+		.event-tiles {
 			flex-direction: row;
 		}
-		.img-race img {
-			width: 40vw;
+		.event-header {
+			flex: 1;
+		}
+		.event-tiles > img {
+			flex: 1;
+			display: block;
+			width: 50vw;
+			aspect-ratio: 5/2.5;
+		}
+		.event-tiles:nth-child(2) {
+			flex-direction: row-reverse
 		}
 	}
 
@@ -947,28 +637,16 @@
 		#howitworks {
 			margin-inline: 0.5rem;
 		}
-		.free-practice .fp-sessions img,
-		.qualifying .quali-sessions img {
-			width: 80vw;
-		}
-
-		.fp-sessions,
-		.quali-sessions {
-			display: flex;
-			flex-direction: column;
-			gap: 2rem;
-		}
-
-		.layout-race {
-			flex-direction: column;
-			align-items: center;
-			gap: 3rem;
-		}
-		.img-race {
+		.event-tiles {
 			flex-direction: column;
 		}
-		.img-race img {
-			width: 80vw;
+		.event-tiles:nth-child(2) {
+			flex-direction: column;
+		}
+		.event-tiles > img {
+			flex: 1;
+			width: 65vw;
+			aspect-ratio: 5/2.5;
 		}
 	}
 </style>
